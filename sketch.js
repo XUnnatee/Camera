45c7ff96aa78ae29859938f13b2f18d005d93e17
XUnnatee <98157500+XUnnatee@users.noreply.github.com>
2023-01-27 21:39:08 +0530

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  canvas = createCanvas(640, 480); 
+  canvas = canvas.position(110,250);
+  video = createCapture(VIDEO);
+  video.hide();
+}
+ function draw() {
+  image(video, 0,0,640,480);
+ }
+function take_snapshot(){
+  save('student_name.png');
+}
